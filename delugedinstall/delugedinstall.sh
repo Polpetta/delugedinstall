50fb0f59fe3fd201aeed07edb3ffcb777c95d2c5
Davide Polonio <poloniodavide@gmail.com>
2013-11-22 19:04:29 +0100

    Script translated in English

--- delugedinstall/delugedinstall.sh
+++ delugedinstall/delugedinstall.sh
@@ -11,7 +11,7 @@ for tmp in `cat /tmp/Aj88h5`
 	versionescaricata[i]=$tmp
 	if [ $i == 0 ]
 		then
-		echo "|"`date +%T`"|" "L'ultima versione disponibile è la $versionescaricata e si possiede la versione $versione."
+		echo "|"`date +%T`"|" "Last available version is $versionescaricata version, and you currently have $versione version."
 		sleep 1
 	fi
 	let i=$i+1
@@ -19,12 +19,12 @@ for tmp in `cat /tmp/Aj88h5`
 rm /tmp/Aj88h5
 if [ ${versionescaricata[0]} == $versione ]
 	then
-	echo "|"`date +%T`"|" "Disponete dell'ultima versione. Lo script non necessita di nessun aggiornamento."
-	echo "|"`date +%T`"|" "Inizierà l'installazione del demone di Deluge."
+	echo "|"`date +%T`"|" "You have the last available version. The script doesn't need any update."
+	echo "|"`date +%T`"|" "Deluge daemon's installation will start now."
 else
-	echo "|"`date +%T`"|" "È necessario un aggiornamento!"
+	echo "|"`date +%T`"|" "An update is needed!"
 	sleep 1
-	echo "|"`date +%T`"|" "Potete trovare il changelog della nuova versione al seguente link: ${versionescaricata[2]}"
+	echo "|"`date +%T`"|" "You can find new version's changelog at the following link: ${versionescaricata[2]}"
 	sleep 2
 	wget ${versionescaricata[1]}
 	chmod +x delugedinstall$versionescaricata.sh
@@ -33,67 +33,67 @@ fi
 }
 echo "###############################################################"
 echo "#                                                             #"
-echo "#           Deluge Installer per sistemi Debian-like          #"
+echo "#           Deluge Installer for Debian-like systems          #"
 echo "#                         v $versione                               #"
 echo "#                                                             #"
 echo "###############################################################"
-echo "#Note:                                                        #"
-echo "#§Per eseguire alcuni comandi è necessario la password di     #"
-echo "# amministrazione e una connessione a internet.               #"
-echo "#§Verranno rimossi e modificati dei pacchetti. Si consiglia   #"
-echo "# aver disponibile come minimo 100 MB sul disco.              #"
-echo "#§La procedura potrebbe richiedere tempo. Si prega di         #"
-echo "# attendere pazientemente l'esecuzione dello script.          #"
-echo "#§Questa procedura vi consentirà l'installazione del client   #"
-echo "# torrent Deluge come demone nel vostro computer.             #"
-echo "# Utilizzatelo con le dovute precauzioni.                     #"
-echo "#§È possibile terminare in qualsiasi momento lo script        #"
-echo "# premendo simultaneamente Ctrl+c.                            #"
+echo "#Notes:                                                       #"
+echo "#§In order to run some commands you will need administrator's #"
+echo "#  password and an Internet connection.                       #"
+echo "#§Some package will be removed or modified. We recommend      #"
+echo "# to have at least 100 MB of free disk space.                 #"
+echo "#§The procedure could teke some time. Please, patiently wait  #"
+echo "#  script execution.                                          #"
+echo "#§This procedure will allow you to install on your computer   #"
+echo "# Deluge, a torrent client.                                   #"
+echo "# We recommend to use it with proper precautions.             #"
+echo "#§It's possible to stop the script in any moment simply       #"
+echo "# pressing Ctrl+c at the same time.                           #"
 echo "###############################################################"
-echo "#§Prima di iniziare, è necessario dare allo script il vostro  #"
-echo "# username e la vostra password, richieste nel processo di    #"
-echo "# configurazione di Deluge. Esse serviranno solamente per     #"
-echo "# configurare correttamente il demone.                        #"
+echo "#§Before starting, it's necessary to give to the script       #"
+echo "# your username and password, that are requested in Deluge's  #"
+echo "# configuration process. It will only be used to              #"
+echo "# correctly configurate the daemon.                           #"
 echo "###############################################################"
 sleep 3
-echo "|"`date +%T`"|" "Controllo l'esistenza di una versione più aggiornata dello script..."
+echo "|"`date +%T`"|" "Checking for the existance of a newer version of the script..."
 sleep 1
 controlloversione
 read -p "Username: " username
 echo "Password per $username: " ; read -s password
-echo "|"`date +%T`"|" "Inizio esecuzione dello script..."
-echo "|"`date +%T`"|" "Cerco il sistema operativo utilizzato..." `uname -v`
-echo "|"`date +%T`"|" "Installo Deluge con l'interfaccia web e dipendenze..."
+echo "|"`date +%T`"|" "Starting script's execution..."
+echo "|"`date +%T`"|" "Checking what OS is used..." `uname -v`
+echo "|"`date +%T`"|" "Installing Deluge with web interface and dependances..."
 sleep 1
 sudo apt-get update
 sudo apt-get install deluged python-mako deluge-web -y
-echo "|"`date +%T`"|" "Avvio Deluge..."
+echo "|"`date +%T`"|" "Starting Deluge..."
 deluged
 sleep 5
-echo "|"`date +%T`"|" "Modifico il file di configurazione"
-echo "|"`date +%T`"|" "Nota: sarà creato un backup del file ~/.config/deluge/auth. Sarà rinominato auth.old"
+echo "|"`date +%T`"|" "Confiuration's file will be modified"
+echo "|"`date +%T`"|" "Note: a backup of the file ~/.config/deluge/auth will be created. it will be renamed auth.old"
 cp ~/.config/deluge/auth ~/.config/deluge/auth.old
 sudo echo "$username:$password:10" >> ~/.config/deluge/auth
-echo "|"`date +%T`"|" "Avvio interfaccia Web di Deluge..."
+echo "|"`date +%T`"|" "Initiating Deluge's web interface..."
 deluge-web > /dev/null &
-echo "|"`date +%T`"|" "Creo cartella per i download in /home/$username/Download..."
+echo "|"`date +%T`"|" "Creating a download folder in /home/$username/Download..."
 mkdir /home/$username/Download
 mkdir /home/$username/Download/.temp
-echo "|"`date +%T`"|" "Configuro Deluge per l'avvio automatico..."
+echo "|"`date +%T`"|" "Configuring Deluge for auto start..."
 sleep 1
 sudo wget -O /etc/default/deluge-daemon http://dl.delugedinstall.altervista.org/dl/permanent/default.deluge-daemon.txt
 sudo sed -i "4 i DELUGED_USER=\"$username\"" /etc/default/deluge-daemon
 sudo wget -O /etc/init.d/deluge-daemon http://dl.delugedinstall.altervista.org/dl/permanent/init.d.deluge-daemon.txt
 sudo chmod 755 /etc/init.d/deluge-daemon
 sudo update-rc.d deluge-daemon defaults
-read -p "|"`date +%T`"| Installazione completata. Si desidera riavviare il sistema? (y/n)?" risposta
+read -p "|"`date +%T`"| Installation complete. Do you want to reboot the system? (y/n)?" risposta
 if [ "$risposta" = "y" ]
 then
-	echo "Il sistema verrà riavviato!"
+	echo "System will reboot!"
 	sleep 1
 	sudo reboot
 else
-	echo "Installazione finita!"
+	echo "Installation complete!"
 fi
 
 
